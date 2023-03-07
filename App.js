6a113032a2ffddcfaa1806a48a72f56dfe13d6a8
DODAN9 <9139a90@naver.com>
2023-03-07 17:14:30 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [userText, setUserText] = useState("");
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  const onChangeInputText = (payload) => {
+    setUserText(payload);
+  };
+
+  useEffect(() => {}, [working]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
+      <View style={styles.header}>
+        <TouchableOpacity activeOpacity={0.7} onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onChangeText={onChangeInputText}
+          value={userText}
+          placeholderTextColor={theme.grey}
+          placeholder={working ? "add to do" : "where you want to go?"}
+          style={styles.input}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +59,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+    paddingHorizontal: 20,
+  },
+  header: {
+    marginTop: 100,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  btnText: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: 600,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
